6c054d445e28d8058497e8b842b8711f3628075a
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2023-01-10 22:14:15 -0300

    Melhoria no script de Inicialização

--- Lag_profile.ps1
+++ Lag_profile.ps1
@@ -1,2 +1,23 @@
 Import-Module -Name ".\NddLibrary.ps1" --verbose;
-Import-Module -Name ".\ProfileBackup.ps1" --verbose;
+Import-Module -Name ".\Lag-variable.ps1" --verbose;
+
+# PSReadLine
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineOption -HistorySearchCursorMovesToEnd
+Set-PSReadlineOption -ShowToolTips
+
+# Functions
+function set-PS7 { ."$($env:ProgramFiles)\powershell\7\pwsh.exe" }
+function ssms { ."$(${env:ProgramFiles(x86)})\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe" }
+function dockerdesktop { ."$($env:ProgramFiles)\Docker\Docker\Docker Desktop.exe" }
+function queue { ."$(${env:ProgramFiles(x86)})\Jarbas\QueueViewer\QueueViewer.exe" }
+function dmozinho { ."C:\MoveInteractive\LDMO\DMLauncher.exe" }
+
+oh-my-posh init pwsh --config $omp.fileTheme | Invoke-Expression
+
+clear
+
+Write-Output "Terminal ON FIREEE"
+Write-Output "Bem vindo a base de controle."
