d483f42b93d59dddadb23b1705101f528ccf4ad1
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-01 13:03:33 -0300

    Implementação do Get-Lag-Variables

--- LagVariableLibrary.ps1
+++ LagVariableLibrary.ps1
@@ -17,5 +17,31 @@ function Get-Lag-Variables {
         return;
     }
 
-    Write-Output 'Ok!';
+    Write-Output 'File .lag Ok!';
+
+    try {
+        $fileContent = [System.IO.File]::ReadAllText($filePath) | ConvertFrom-Json;
+
+        $fileContent.psobject.properties | ForEach-Object {
+            try {
+                Write-Output "Consultando variavel: $($_.Name)"
+                Set-Variable -Name $_.Name -Value $_.Value -Scope Global; 
+            }
+            catch [System.Management.Automation.ItemNotFoundException] {
+                Write-Output "Status: Nova"
+                New-Variable -Name $_.Name -Value $_.Value -Scope Global;
+            }
+            catch [System.Exception] {
+                Write-Error 'Ocorreu um erro inesperado!'
+                Write-Error $_.Exception.Message;
+            }
+        }  
+    }
+    catch {
+        Write-Error 'Ocorreu um erro inesperado!'
+        Write-Error $_.Exception.Message;
+    }
+    finally {
+        Write-Output 'Finish!'
+    }
 }
