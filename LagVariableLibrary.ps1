0f9596dbd99f53742e267a20d3e0a2414157303b
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-01 21:53:27 -0300

    Melhoria na biblioteca de Variaveis LAG

--- LagVariableLibrary.ps1
+++ LagVariableLibrary.ps1
@@ -1,4 +1,13 @@
-function Get-Lag-Variables {
+<#
+    .Synopsis
+       Adiciona as Variaveis conforme o arquivo .Lag
+    .DESCRIPTION
+       Converte o arquivo .Lag em Variaveis Globais para serem utilizadas no contexto atual
+    .EXAMPLE
+       Push-Lag-Variables-File
+#>
+function Push-Lag-Variables-File {
+
     param ( 
         [Parameter(Position=0)]
         [string]
@@ -6,8 +15,28 @@ function Get-Lag-Variables {
     )
 
     if ([string]::IsNullOrEmpty($path)) {
-        $Path = Set-Location;
+        $Path = $Home;
     }
+
+    $variables = Get-Lag-Variables-File -Path $Path;
+
+    $variables.psobject.properties | ForEach-Object { Add-Lag-Variable $_.Name $_.Value; }
+}
+
+<#
+    .Synopsis
+       Obtem as Variaveis do arquivo .Lag
+    .DESCRIPTION
+       Converte o arquivo .Lag em um PSCustomObject
+    .EXAMPLE
+       Get-Lag-Variables-File -Path "C:\temp"
+#>
+function Get-Lag-Variables-File {
+    param ( 
+        [Parameter(Mandatory)]
+        [string]
+        $Path
+    )
 
     $file = '.lag';
     $filePath = [string]::Concat($path, '/', $file);
@@ -17,31 +46,46 @@ function Get-Lag-Variables {
         return;
     }
 
-    Write-Output 'File .lag Ok!';
-
     try {
-        $fileContent = [System.IO.File]::ReadAllText($filePath) | ConvertFrom-Json;
-
-        $fileContent.psobject.properties | ForEach-Object {
-            try {
-                Write-Output "Consultando variavel: $($_.Name)"
-                Set-Variable -Name $_.Name -Value $_.Value -Scope Global; 
-            }
-            catch [System.Management.Automation.ItemNotFoundException] {
-                Write-Output "Status: Nova"
-                New-Variable -Name $_.Name -Value $_.Value -Scope Global;
-            }
-            catch [System.Exception] {
-                Write-Error 'Ocorreu um erro inesperado!'
-                Write-Error $_.Exception.Message;
-            }
-        }  
+        Write-Verbose 'Get File Text and Convert to PSCustomObject'
+        return [System.IO.File]::ReadAllText($filePath) | ConvertFrom-Json;
     }
     catch {
         Write-Error 'Ocorreu um erro inesperado!'
         Write-Error $_.Exception.Message;
+        throw $_;
     }
-    finally {
-        Write-Output 'Finish!'
+}
+
+<#
+    .Synopsis
+       Adiciona a Variavel no contexto Global
+    .DESCRIPTION
+       Adiciona PSCustomObject como Variavel no Contexto Global
+    .EXAMPLE
+       Add-Lag-Variable 'Projetos' 'C:\Projetos' }
+#>
+function Add-Lag-Variable {
+    param (
+        # Nome da Variavel
+        [Parameter(Mandatory, Position=0)]
+        [string]
+        $Key,
+
+        # Valor da Variavel
+        [Parameter(Mandatory, Position=2)]
+        [System.Object]
+        $Value
+    )
+
+    try {
+        Write-Verbose "Add Variable: $key"
+        New-Variable -Name $Key -Value $Value -Scope Global;
     }
+    catch [System.Exception] {
+        Write-Error 'Ocorreu um erro inesperado!'
+        Write-Error $_.Exception.Message;
+    }
+
+    Write-Verbose 'Variables Ok!';
 }
