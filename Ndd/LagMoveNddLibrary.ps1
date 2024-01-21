06a4ed7e089984c14f498bde998f24bf557313b4
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-21 20:08:37 -0300

    Remove ParticularSoftware (MOVE)

--- Ndd/LagMoveNddLibrary.ps1
+++ Ndd/LagMoveNddLibrary.ps1
@@ -1,3 +1,22 @@
+using namespace System.IO;
+
+function Remove-ParticularSoftware {
+
+    $particularSoftwarePath = [Path]::Combine($env:LOCALAPPDATA, 'ParticularSoftware');
+    Write-Output $particularSoftwarePath
+
+    if (Test-Path -Path $particularSoftwarePath)
+    {
+        Write-Output "Excluindo pasta";
+        Remove-Item -Path $particularSoftwarePath -Force;
+    }
+    else
+    {
+        Write-Output 'ParticularSoftware não encontrado!';
+        return;
+    }
+}
+
 function Get-Token-Portal {
     $body = @{
         grant_type='client_credentials'
