8406cad19e39e9733b60bf108507d75b14aa3f6b
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-02-06 20:46:53 -0300

    Comentado validação da Data Criação no Set-Certificates

--- Ndd/Libs/LagMoveNddLibrary.ps1
+++ Ndd/Libs/LagMoveNddLibrary.ps1
@@ -5,13 +5,11 @@ function Remove-ParticularSoftware {
     $particularSoftwarePath = [Path]::Combine($env:LOCALAPPDATA, 'ParticularSoftware');
     Write-Output $particularSoftwarePath
 
-    if (Test-Path -Path $particularSoftwarePath)
-    {
+    if (Test-Path -Path $particularSoftwarePath) {
         Write-Output "Excluindo pasta";
         Remove-Item -Path $particularSoftwarePath -Force;
     }
-    else
-    {
+    else {
         Write-Output 'ParticularSoftware não encontrado!';
         return;
     }
@@ -19,9 +17,9 @@ function Remove-ParticularSoftware {
 
 function Get-Token-Portal {
     $body = @{
-        grant_type='client_credentials'
-        client_id='7afbb7b3a0ab4ede893e2f9490e9ffcf'
-        client_secret='ibtwgrHna+thf+p9wkqdo7M250zyynUTwYXA72lPWC4='
+        grant_type    = 'client_credentials'
+        client_id     = '7afbb7b3a0ab4ede893e2f9490e9ffcf'
+        client_secret = 'ibtwgrHna+thf+p9wkqdo7M250zyynUTwYXA72lPWC4='
     }
     
     $contentType = 'application/x-www-form-urlencoded' 
@@ -30,7 +28,7 @@ function Get-Token-Portal {
     
     $json = $request.Content | ConvertFrom-Json
 
-    $bearer =  "Bearer $($json.access_token)"; 
+    $bearer = "Bearer $($json.access_token)"; 
 
     Set-Clipboard -Value $bearer
 
@@ -39,17 +37,17 @@ function Get-Token-Portal {
 
 function Get-Token-Mobile {
     $body = @{
-        client_id='3b9a77fb35a54e40815f4fa8641234c5'
-        grant_type='password'
-        userName='11484671902'
-        password='12345678'
+        client_id  = '3b9a77fb35a54e40815f4fa8641234c5'
+        grant_type = 'password'
+        userName   = '11484671902'
+        password   = '12345678'
     }
     
     $request = Invoke-WebRequest -Method POST -Uri 'http://localhost:9002/token' -body $body
     
     $json = $request.Content | ConvertFrom-Json
 
-    $bearer =  "Bearer $($json.AccessToken)"; 
+    $bearer = "Bearer $($json.AccessToken)"; 
 
     Set-Clipboard -Value $bearer
 
@@ -63,26 +61,31 @@ function Set-Certificates {
     $certBackupPath = "$($Ndd.Move)/Certificados Identity/*";    
     $certBackup = Get-ChildItem -Path $certBackupPath -Include $certificates;
     
-    if ($certBackup.Length -le 2) 
-    {
+    if ($certBackup.Length -le 2) {
         Write-Output 'Não foi encontrado todos os certificados necessários';
         return;
     }
 
     $certActualPath = "$($Polaris.Path)/configs/identity/*";
-    $certActual = Get-ChildItem -Path $certActualPath -Include $certificates;
-
-    if ($certActual.Length -ge 1)
-    {
-        $maxBackupDate = $certBackup | Measure-Object -Property LastAccessTime -Maximum;
-        $maxActualDate = $certActual | Measure-Object -Property LastAccessTime -Maximum;
     
-        if ($maxActualDate.Maximum -gt $maxBackupDate.Maximum)
+    # Removido temporariamente
+    # Validação devo ocorrer apenas quando os certificados forem Untracked
+
+    <#    
+        $certActual = Get-ChildItem -Path $certActualPath -Include $certificates;
+
+        if ($certActual.Length -ge 1)
         {
-            Write-Output 'Certificados do Backup estão desatualizados!';
-            return;
+            $maxBackupDate = $certBackup | Measure-Object -Property LastAccessTime -Maximum;
+            $maxActualDate = $certActual | Measure-Object -Property LastAccessTime -Maximum;
+        
+            if ($maxActualDate.Maximum -gt $maxBackupDate.Maximum)
+            {
+                Write-Output 'Certificados do Backup estão desatualizados!';
+                return;
+            }
         }
-    }
+    #>
 
     Remove-Item -Path $certActualPath -Include $certificates -Force;
     
@@ -107,13 +110,12 @@ function Connect-Polaris {
 
         [switch]
         $lone1, 
-        
+
         [switch]
         $lone3
     )
     
-    if ($lone1) 
-    {
+    if ($lone1) {
         $username = 'lonestar';
         $ip = '20.55.122.52';
         $key = 'C:/Ndd/Move/SSH Ambientes/Lonestar1.ppk'
