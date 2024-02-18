448dd0212422b7805fa05e83cc23b1ca47a914e3
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-02-17 22:38:52 -0300

    Enhancement to the Get-RabbitQueues command

--- Ndd/Libs/LagMoveRabbitMqNddLibrary.ps1
+++ Ndd/Libs/LagMoveRabbitMqNddLibrary.ps1
@@ -3,11 +3,9 @@ using namespace System.Text;
 
 # Configurações RabbitMQ
 $rabbitUrl = "http://localhost:15672";
-$user = "guest";
-$pass = "guest";
-
-$base64AuthInfo = [Convert]::ToBase64String([Encoding]::ASCII.GetBytes("${user}:${pass}"));
+$rabbitBase64Auth = [Convert]::ToBase64String([Encoding]::ASCII.GetBytes("guest:guest"));
 
 function Get-RabbitQueues() {
-    return Invoke-RestMethod -Uri "${rabbitUrl}/api/queues" -Headers @{ Authorization = "Basic $base64AuthInfo" } -Method Get
+    return Invoke-RestMethod -Uri "${rabbitUrl}/api/queues" -Headers @{ Authorization = "Basic $rabbitBase64Auth" } -Method Get | 
+            ForEach-Object { $_.Name };
 }
