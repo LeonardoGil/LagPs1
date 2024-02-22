f9e63c4bc671ccc2226a9536623bd54ceae945b2
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-02-21 22:09:13 -0300

    Removing deprecated functions

--- src/profiles/LagProfile.ps1
+++ src/profiles/LagProfile.ps1
@@ -22,7 +22,6 @@ Set-PSReadlineOption -ShowToolTips
 
 # Alias
 New-Alias -Name 'ex' -Value explorer;
-New-Alias -Name 'ldir' -Value Get-Lag-ChildItem;
 
 function Initialize-Lag {
     # Push-Lag-Variables-File;
@@ -30,44 +29,6 @@ function Initialize-Lag {
     
     Write-Host "Terminal ON FIREEE" -ForegroundColor Red;
     Write-Output "Bem vindo a base de controle.";
-}
-
-function Start-Lag-App {
-    param ( 
-        [switch]
-        $PowerShellSeven,
-
-        [switch]
-        $SSMS,
-
-        [switch]
-        $DockerDesktop,
-
-        [switch]
-        $Queue,
-
-        [switch]
-        $Binzinho,
-
-        [switch]
-        $inator
-    )
-
-    if ($PowerShellSeven) { Start-Process "$($env:ProgramFiles)\powershell\7\pwsh.exe"; }
-
-    if ($SSMS) { Start-Process "$(${env:ProgramFiles(x86)})\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"; }
-
-    if ($DockerDesktop) { Start-Process "$($env:ProgramFiles)\Docker\Docker\Docker Desktop.exe"; }
-
-    if ($Queue) { Start-Process "$(${env:ProgramFiles(x86)})\Jarbas\QueueViewer\QueueViewer.exe"; }
-
-    if ($Binzinho) { Start-Process "$(${env:ProgramFiles(x86)})\Bodega\BinKiller\BinKiller.exe"; }
-
-    if ($inator) { Start-Process "$($ndd.move)\MoveInator\MoveInatorForms.exe"; }
-}
-
-function Get-Lag-ChildItem {
-    (Get-ChildItem) | Select-Object -Property BaseName, CreationTime, Extension
 }
 
 Initialize-Lag
