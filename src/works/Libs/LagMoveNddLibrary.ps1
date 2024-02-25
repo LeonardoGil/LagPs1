22b552a8c7193fe8b98a41cc36ea4afcacb6329f
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-02-24 22:14:50 -0300

    Adjustment on the Set-Certificates

--- src/works/Libs/LagMoveNddLibrary.ps1
+++ src/works/Libs/LagMoveNddLibrary.ps1
@@ -56,23 +56,47 @@ function Get-Token-Mobile {
 
 function Set-Certificates {
     
+    param (
+        [string]
+        $certificatesPath = (Get-Location),
+
+        [Parameter(Mandatory)]
+        [string]
+        $projectPath
+    )
+
     $certificates = @("identity-cert.pfx", "identity-root-cert.cer", "identity-root-cert.crt");
-    
-    $certBackupPath = "$($Ndd.Move)/Certificados Identity/*";    
-    $certBackup = Get-ChildItem -Path $certBackupPath -Include $certificates;
-    
-    if ($certBackup.Length -le 2) {
-        Write-Output 'Não foi encontrado todos os certificados necessários';
+    $identityProjectPath = "$projectPath/configs/identity";
+
+    if (-not(Test-Path $certificatesPath)) {
+        Write-Output 'Caminho do Certificado inválido';
         return;
     }
 
-    $certActualPath = "$($Polaris.Path)/configs/identity/*";
-    
-    Remove-Item -Path $certActualPath -Include $certificates -Force;
-    
-    Copy-Item -Path $certBackupPath -Include $certificates -Destination "$($Polaris.Path)/configs/identity";
+    if (-not(Test-Path $projectPath)) {
+        Write-Output 'Caminho do Projeto inválido';
+        return;
+    }
 
-    Write-Output 'Certificados atualizados!';
+    try {
+        # Verifica se possui todos os 3 certificados necessario na pasta
+        if ((Get-ChildItem -Path "$certificatesPath/*" -Include $certificates).Length -le 2) {
+            
+            Write-Output 'Não foi encontrado todos os certificados necessários';
+            return;
+        }
+
+        # Limpa os certificados do Projeto
+        Remove-Item -Path "$identityProjectPath/*" -Include $certificates -Force;
+
+        # Clona os certificados para Projeto
+        Copy-Item -Path "$certificatesPath/*" -Include $certificates -Destination $identityProjectPath;
+
+        Write-Output 'Certificados atualizados!';
+    }
+    catch {
+        Write-Output "Ocorreu um erro. $($_.message)"
+    }
 }
 
 function Connect-Polaris {
