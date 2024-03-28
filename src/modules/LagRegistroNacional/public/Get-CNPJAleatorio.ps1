5d07c12e181299b6787be033d9154815390ad662
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-03-27 21:33:26 -0300

    Adjustments to the return of the random CNPJ and CPF get methods

--- src/modules/LagRegistroNacional/public/Get-CNPJAleatorio.ps1
+++ src/modules/LagRegistroNacional/public/Get-CNPJAleatorio.ps1
@@ -1,25 +1,23 @@
 function Get-CNPJAleatorio() {
-    $random = [System.Random]::new();
-    $cnpj = [string]::Empty;
+    $random = [System.Random]::new()
+    $cnpj = [string]::Empty
 
     # Gera um cnpj Aleatorio (sem o Digito Verificador) 
     for ($i = 0; $i -lt 8; $i++) {
-        $number = $random.Next(0, 9);
-
-        $cnpj = -join ($cnpj, $number);
+        $number = $random.Next(0, 9)
+        $cnpj = -join ($cnpj, $number)
     }
 
     $cnpj += "0001"
 
-    $digitoVerificador = Get-DigitoVerificador $cnpj 9;
+    $digitoVerificador = Get-DigitoVerificador $cnpj 9
+    $cnpj = -join ($cnpj, $digitoVerificador)
 
-    $cnpj = -join ($cnpj, $digitoVerificador);
-
-    $digitoVerificador = Get-DigitoVerificador $cnpj 9;
-
-    $cnpj = -join ($cnpj, $digitoVerificador);
+    $digitoVerificador = Get-DigitoVerificador $cnpj 9
+    $cnpj = -join ($cnpj, $digitoVerificador)
 
     Set-Clipboard -Value $cnpj
+    Write-Verbose "cnpj: $cnpj"
 
-    Write-Output "cnpj: $cnpj";
+    return $cnpj
 }
--- src/modules/LagRegistroNacional/public/Get-CPFAleatorio.ps1
+++ src/modules/LagRegistroNacional/public/Get-CPFAleatorio.ps1
@@ -1,23 +1,21 @@
 function Get-CPFAleatorio() {
-    $random = [System.Random]::new();
-    $cpf = [string]::Empty;
+    $random = [System.Random]::new()
+    $cpf = [string]::Empty
 
     # Gera um CPF Aleatorio (sem o Digito Verificador) 
     for ($i = 0; $i -lt 9; $i++) {
-        $number = $random.Next(0, 9);
-
-        $cpf = -join ($cpf, $number);
+        $number = $random.Next(0, 9)
+        $cpf = -join ($cpf, $number)
     }
 
-    $digitoVerificador = Get-DigitoVerificador $cpf 11;
+    $digitoVerificador = Get-DigitoVerificador $cpf 11
+    $cpf = -join ($cpf, $digitoVerificador)
 
-    $cpf = -join ($cpf, $digitoVerificador);
-
-    $digitoVerificador = Get-DigitoVerificador $cpf 11;
-
-    $cpf = -join ($cpf, $digitoVerificador);
+    $digitoVerificador = Get-DigitoVerificador $cpf 11
+    $cpf = -join ($cpf, $digitoVerificador)
 
     Set-Clipboard -Value $cpf
+    Write-Verbose "CPF: $cpf"
 
-    Write-Output "CPF: $cpf";
+    return $cpf
 }
