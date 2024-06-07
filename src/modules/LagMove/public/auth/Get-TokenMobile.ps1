ddeef328a11e58a0cccac0f167c6029d16a3570b
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-06-06 21:48:10 -0300

    Added parameter clientId and fix bugs in Get-TokenMobile

--- src/modules/LagMove/public/auth/Get-TokenMobile.ps1
+++ src/modules/LagMove/public/auth/Get-TokenMobile.ps1
@@ -12,34 +12,56 @@ function Get-TokenMobile {
 
         [Parameter()]
         [System.Uri]
-        $apiUrl
+        $apiUrl,
+
+        [Parameter()]
+        [string]
+        $clientId
     )
 
     $moveSession = [MoveSession]::Get()
 
-    if ([string]::IsNullOrEmpty($cpf)) { $cpf = $moveSession.Mobile.cpf }
-    if ([string]::IsNullOrEmpty($pass)) { $pass = $moveSession.Mobile.password }
+    # validation
 
+    if ($null -eq $moveSession) {
+        Write-Host 'Sessão não definida' -ForegroundColor DarkYellow
+        return
+    }
+
+    if ([string]::IsNullOrEmpty($cpf)) {
+        $cpf = $moveSession.Mobile.cpf 
+    }
+
+    if ([string]::IsNullOrEmpty($pass)) {
+        $pass = $moveSession.Mobile.password 
+    }
+
+    if ([string]::IsNullOrEmpty($clientId)) {
+        $clientId = $moveSession.Mobile.clientId 
+    }
+    
     if ($null -eq $apiUrl) { 
-        $urlBuilder = [System.UriBuilder]::new($mobileSession.Url)
+        $urlBuilder = [System.UriBuilder]::new($moveSession.Url)
         $urlBuilder.Port = [MobileSession]::portAuthDefault
         $apiUrl = $urlBuilder.Uri
     }
 
+    # Request
+
     $body = @{
-        client_id  = $mobileSession.clientId
+        client_id  = $clientId
         grant_type = 'password'
         userName   = $cpf
         password   = $pass
     }
     
-    $ErrorActionPreference = 'Stop'
-
     $url = [System.Uri]::new($apiUrl, 'token');
 
     $request = Invoke-WebRequest -Method POST -Uri $url -body $body
     
     $json = $request.Content | ConvertFrom-Json
+
+    # Result
 
     $bearer = "Bearer $($json.AccessToken)"; 
 
