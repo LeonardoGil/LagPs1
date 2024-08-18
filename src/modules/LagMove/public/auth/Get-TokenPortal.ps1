46e99ce7388be747256e090deec2ec6284300d89
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-08-18 08:07:41 -0300

    Melhoria na Get-TokenPortal

--- src/modules/LagMove/public/auth/Get-TokenPortal.ps1
+++ src/modules/LagMove/public/auth/Get-TokenPortal.ps1
@@ -1,13 +1,42 @@
 function Get-TokenPortal {
-    $body = @{
-        grant_type    = 'client_credentials'
-        client_id     = 'i-comprova-client-sdk'
-        client_secret = 'i-comprova-core-web-api-client-sdk'
-    }
     
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $clientId,
+
+        [Parameter()]
+        [string]
+        $clientSecret,
+
+        [Parameter()]
+        [switch]
+        $default
+    )
+    
+    $body = @{
+        grant_type = 'client_credentials'
+    }
+
+    if ($default.IsPresent) {
+        $body.client_id = 'i-comprova-client-sdk'
+        $body.client_secret = 'i-comprova-core-web-api-client-sdk'
+    }
+    else {
+
+        if ([string]::IsNullOrEmpty($clientId) -or [string]::IsNullOrEmpty($clientSecret)) {
+            Write-Error 'Necessario informar ClientId e ClientSecret'
+            return
+        }
+
+        $body.client_id = $clientId
+        $body.client_secret = $clientSecret
+    }
+
     $contentType = 'application/x-www-form-urlencoded' 
     
-    $request = Invoke-WebRequest -Method POST -Uri 'https://host.docker.internal:5001/connect/token' -body $body -ContentType $contentType
+    $request = Invoke-WebRequest -Method POST -Uri 'https://host.docker.internal:5001/connect/token' -body $body -ContentType $contentType 
     
     $json = $request.Content | ConvertFrom-Json
 
