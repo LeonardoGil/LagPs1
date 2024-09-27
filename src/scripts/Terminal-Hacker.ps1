ee1c777771a553985c200b8f80a4b97d107a04a0
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-09-27 05:52:33 -0300

    Ajustes terminal-matrix

--- src/scripts/Terminal-Hacker.ps1
+++ src/scripts/Terminal-Hacker.ps1
@@ -1,45 +1,51 @@
 $Host.UI.RawUI.BufferSize = $Host.UI.RawUI.WindowSize
 
 $strings = @(
-    "Arrou",
-    "Selou",
-    "Naruto",
-    "Falencia",
-    "Cruel",
-    "Ogroo",
-    "Moreia",
-    "Intelectual",
-    "Anaconda",
-    "Trampolim",
-    "Viet",
-    "Golias",
-    "Padre"
+    "Password",
+    "Thread",
+    "User",
+    "Coding",
+    "Debbug",
+    "Writting",
+    "Analysis",
+    "Compiler",
+    "Framework",
+    "Algorithm",
+    "Function",
+    "Runtime",
+    "Parameter",
+    "Inheritance",
+    "Library",
+    "Module",
+    "Package"
 )
 
 $exec = @()
 
-for ($i = 0; $i -lt 100; $i++) {
+for ($i = 0; $i -lt 900; $i++) {
 
     $randomString = Get-Random -Minimum 0 -Maximum ($strings.Count - 1)
-    $randomColumn = Get-Random -Minimum 1 -Maximum ($Host.UI.RawUI.BufferSize.Width / 2)
+    $randomColumn = Get-Random -Minimum 1 -Maximum ($Host.UI.RawUI.BufferSize.Width -1)
 
     if ($strings[$randomString] -notcontains ($exec | Select-Object -Property string)) {
         if ($randomColumn -notcontains ($exec | Select-Object -Property column)) {
-            $item = [PSCustomObject]@{
-                column = $randomColumn
-                position = 0
-                string = $strings[$randomString]
+            if ((Get-Random -Minimum 0 -Maximum 9) -le 8) {
+                $item = [PSCustomObject]@{
+                    column = $randomColumn
+                    position = 0
+                    string = $strings[$randomString]
+                }
+                $exec += $item
             }
-            $exec += $item
         }
     }
 
+    $exec = @($exec | Where-Object { $_.position -ne $_.string.Length } | Sort-Object column) 
+
     $result = ''
 
-    $exec = @($exec | Where-Object { $_.position -ne ($_.string.Length -1) } | Sort-Object column) 
-
     foreach ($obj in $exec) {
-        
+
         $spaces = $obj.column - $result.Length
 
         if ($spaces -gt 0) {
@@ -51,7 +57,6 @@ for ($i = 0; $i -lt 100; $i++) {
         $result += $obj.string[$obj.position]
         $obj.position += 1
     }
-
     Write-Output $result
-    Start-Sleep -Milliseconds 30
+    Start-Sleep -Milliseconds 15
 } 
