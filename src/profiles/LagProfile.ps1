dce7819ca9f3f5fa906a2c64994ae5fab4e381d3
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-11-03 09:34:34 -0300

    Melhoria no LagProfile

--- src/profiles/LagProfile.ps1
+++ src/profiles/LagProfile.ps1
@@ -1,30 +1,22 @@
-$psProjectPath = 'C:\Projetos\LagPs1\src'
+$lagVerbose     = $false
+$psProjectPath  = 'C:\Projetos\LagPs1\src\modules'
 
-$lagVerbose = $false
-
-# Local Modules 
-Import-Module -Name "$psProjectPath\modules\LagVariable\LagVariable.psm1" -Verbose:$lagVerbose
-Import-Module -Name "$psProjectPath\modules\LagRegistroNacional\LagRegistroNacional.psm1" -Verbose:$lagVerbose
-# Import-Module -Name "$psProjectPath\modules\LagRabbitManager\LagRabbitManager.psm1" -Verbose:$lagVerbose
-# Import-Module -Name "$psProjectPath\modules\LagSQL\LagSQL.psm1" -Verbose:$lagVerbose
-# Import-Module -Name "$psProjectPath\modules\LagAz\LagAz.psm1" -Verbose:$lagVerbose
+Import-Module -Name "$psProjectPath\LagVariable\LagVariable.psm1"   -Verbose:$lagVerbose
+Import-Module -Name "$psProjectPath\LagUtil\LagUtil.psd1"           -Verbose:$lagVerbose
 
 # Third-Party Modules
 Import-Module -Name Terminal-Icons
 
-# PSReadLine Keys
+# PSReadLine
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-# PSReadLine Options
 Set-PSReadlineOption -HistorySearchCursorMovesToEnd
 Set-PSReadlineOption -ShowToolTips
-Set-PSReadlineOption -BellStyle None  # Desabilita campainha
+Set-PSReadlineOption -BellStyle None
 Set-PSReadLineOption -HistorySavePath "C:\Temp\$($Host.Name)_history.txt"
 
 function Initialize-Lag {
-    # Load the lag Variables for session
     Push-LagVariablesFile
     
     # Load Theme of Oh-my-Posh
@@ -38,28 +30,3 @@ Initialize-Lag
 
 # Alias
 New-Alias -Name 'ex' -Value explorer
-
-function Start-Modules {
-    param (
-        [Parameter()]
-        [switch]
-        $Az,
-        
-        [Parameter()]
-        [switch]
-        $Rabbitmq,
-
-        [Parameter()]
-        [switch]
-        $Sql,
-
-        [Parameter()]
-        [switch]
-        $Util
-    )
-
-    if ($Rabbitmq) { Import-Module -Name "$psProjectPath\modules\LagRabbitManager\LagRabbitManager.psm1" }
-    if ($Sql) { Import-Module -Name "$psProjectPath\modules\LagSQL\LagSQL.psm1" }
-    if ($Az) { Import-Module -Name "$psProjectPath\modules\LagAz\LagAz.psm1" }
-    if ($Util) { Import-Module -Name "$psProjectPath\modules\LagUtil\LagUtil.psd1" }
-}
